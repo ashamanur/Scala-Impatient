c8a1683422d01becadf9626d4c93fede492ea82a
Anusha Shamanur <anushasb11@gmail.com>
2016-01-20 15:43:59 -0800

    Chapter 2

---
+++ Chapter2/Q1.scala
@@ -0,0 +1,11 @@
+//The signum of a number is 1 if the number is positive, –1 if it is negative, and
+//0 if it is zero. Write a function that computes this value
+
+def compute(x: Int): Int  {
+	if(x == 0)
+	0
+	else if(x > 0)
+	1
+	else
+	-1
+}
